4139d9740ab83d1db7b1b08f8d00443c78b8f05c
colonelrascals <pdhildreth@gmail.com>
2017-11-29 11:55:20 -0600

    working calculator

--- main.py
+++ main.py
@@ -1,32 +1,69 @@
 import sys
-from PyQt5.QtWidgets import (
-    QWidget, QApplication, QHBoxLayout, QVBoxLayout, QPushButton)
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 
 
-class MainWindow(QWidget):
+class Button:
+    def __init__(self, text, results):
+        self.b = QPushButton(str(text))
+        self.text = text
+        self.results = results 
+        self.b.clicked.connect(lambda: self.handleInput(self.text))
+
+    def handleInput(self, v):
+        if v == '=':
+            res = eval(self.results.text())
+            self.results.setText(str(res))
+        elif v == 'AC':
+            self.results.setText('')
+        else:
+            current_value = self.results.text()
+            new_value = current_value + str(v)
+            self.results.setText(new_value)
+
+
+class Application(QWidget):
     def __init__(self):
         super().__init__()
-        self.init_ui()
+        self.setWindowTitle('Python Calculator')
+        self.CreateApp()
 
-    def init_ui(self):
-        okButton = QPushButton('OK')
-        cancelButton = QPushButton('Cancel')
+    def CreateApp(self):
+        grid = QGridLayout()
+        results = QLineEdit()
 
-        horizontal = QHBoxLayout()
-        horizontal.addStretch()
+        buttons = ['AC', 'C', 'CE', '/',
+                   7, 8, 9, '*',
+                   4, 5, 6, '-',
+                   1, 2, 3, '+',
+                   0, '.', '=']
+        row = 1
+        col = 0
 
-        horizontal.addWidget(okButton)
-        horizontal.addWidget(cancelButton)
+        grid.addWidget(results, 0, 0, 1, 4)
 
-        self.setLayout(horizontal)
+        for button in buttons:
+            # skips rows appropriatly
+            if col > 3:
+                col = 0
+                row += 1
+            but = Button(button, results)
+            
+            if button == 0:
+                grid.addWidget(but.b, row, col, 1, 2)
+                col += 1
+            
+            else:
+                grid.addWidget(but.b, row, col, 1, 1)
 
-        self.setWindowTitle('Horizontal Layout')
+            col += 1
+
+        self.setLayout(grid)
         self.show()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-
-    window = MainWindow()
-
+    window = Application()
     sys.exit(app.exec_())
